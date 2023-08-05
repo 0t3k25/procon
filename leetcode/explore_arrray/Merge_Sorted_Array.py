201ff4591987ec6a88473b7abd85b4fb3e9534d4
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-08-05 10:41:01 +0900

    pointer問題完了

--- leetcode/explore_arrray/Merge_Sorted_Array.py
+++ leetcode/explore_arrray/Merge_Sorted_Array.py
@@ -1,16 +1,21 @@
+# pointer関連の問題
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if len(nums2) > 0:
-            nums2_i = 0
-            for i in range(len(nums1)):
-                if nums1[i] <= nums2[nums2_i]:
-                    nums1.insert(i, nums2[nums2_i])
-                    nums2_i += 1
-                    nums1.pop()
-
-
-# まだ途中
-# 明日やる
+        # ポインタ3つ用意
+        p, p1, p2 = m + n - 1, m - 1, n - 1
+        # 条件分岐
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] >= nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            elif nums2[p2] > nums1[p1]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
